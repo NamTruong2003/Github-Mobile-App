66c3ade541c26648dc62f3116b4bdee810491569
Quoc Huy <quochuy.ws@gmail.com>
2024-09-05 23:17:10 +0700

    create new activity SignInWithGithubActivity

--- app/src/main/java/com/example/githubbrowser/SignInWithGithubActivity.java
+++ app/src/main/java/com/example/githubbrowser/SignInWithGithubActivity.java
@@ -1,8 +1,6 @@
 package com.example.githubbrowser;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,24 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LoginActivity extends AppCompatActivity {
+public class SignInWithGithubActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
-
+        setContentView(R.layout.activity_sign_in_with_github);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        Button signInWithGithubButton = findViewById(R.id.signInWithGithubButton);
-        signInWithGithubButton.setOnClickListener(v -> {
-            Intent intent01 = new Intent(LoginActivity.this, SignInWithGithubActivity.class);
-            startActivity(intent01);
         });
     }
 }
