8bbb17799753a5f16d973ddb9e5797d38e621bd8
jmaqhuy <quochuy.ws@gmail.com>
2024-09-06 06:50:37 +0700

    set action for login button

--- app/src/main/java/com/example/githubbrowser/SignInWithGithubActivity.java
+++ app/src/main/java/com/example/githubbrowser/SignInWithGithubActivity.java
@@ -1,6 +1,8 @@
 package com.example.githubbrowser;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +21,11 @@ public class SignInWithGithubActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        Button signInWithGithubButton = findViewById(R.id.signInButton);
+        signInWithGithubButton.setOnClickListener(v -> {
+            Intent intent01 = new Intent(SignInWithGithubActivity.this, MainActivity.class);
+            startActivity(intent01);
         });
     }
 }
