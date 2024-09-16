8854c585b77a0d06c06190aa2703f18ba226eae5
Quoc Huy <quochuy.ws@gmail.com>
2024-09-16 22:08:00 +0700

    Check username and password aren't empty before login.

--- app/src/main/java/com/example/githubbrowser/SignInWithGithubActivity.java
+++ app/src/main/java/com/example/githubbrowser/SignInWithGithubActivity.java
@@ -3,6 +3,8 @@ package com.example.githubbrowser;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +13,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class SignInWithGithubActivity extends AppCompatActivity {
+    EditText username;
+    EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +26,34 @@ public class SignInWithGithubActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
         Button signInWithGithubButton = findViewById(R.id.signInButton);
+        Button signInWithAPasskeyButton = findViewById(R.id.signInWithAPasskeyButton);
+
+        username = findViewById(R.id.usernameOrEmail);
+        password = findViewById(R.id.password);
+
         signInWithGithubButton.setOnClickListener(v -> {
+            if(username.getText().toString().trim().matches("")){
+                username.setError("Username required!");
+                username.requestFocus();
+                return;
+            }
+            if(password.getText().toString().trim().matches("")){
+                password.setError("Password required!");
+                password.requestFocus();
+                return;
+            }
+
             Intent intent01 = new Intent(SignInWithGithubActivity.this, MainActivity.class);
             startActivity(intent01);
         });
+
+        signInWithAPasskeyButton.setOnClickListener(view -> {
+            Toast.makeText(this, "Passkey sign-in is coming soon!\nStay tuned for updates."
+                    , Toast.LENGTH_SHORT).show();
+        });
+
+
     }
 }
